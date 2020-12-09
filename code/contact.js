72d8d74b9c2dc9d3cce29a4fd1d9b2be5eb199ad
Alfonso Laffont <alaffont@Alfonsos-MBP.fios-router.home>
2020-12-08 19:39:07 -0500

    another JS feature

---
+++ code/contact.js
@@ -0,0 +1,8 @@
+function showDiv1(){
+    if(document.getElementById("find").style.display == 'block'){
+        document.getElementById("find").style.display = 'none';
+    }
+    else{
+        document.getElementById("find").style.display = 'block';
+    }
+}
